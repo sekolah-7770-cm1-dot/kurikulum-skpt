2d04e8423c459dff224197e56bb9fa197ee0d06c
sekolah-7770-cm1-dot <sekolah-7770-cm1@moe-dl.edu.my>
2026-02-05 22:21:06 +0800

    new coding

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   define: {
-    'process.env': process.env
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    }
   }
 });
