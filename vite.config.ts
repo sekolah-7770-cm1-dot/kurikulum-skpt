a3c5e58bb08ce63781e409d27f7ca7662d475983
sekolah-7770-cm1-dot <sekolah-7770-cm1@moe-dl.edu.my>
2026-02-05 22:28:36 +0800

    error coding

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html',
